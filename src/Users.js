c33daf42fc1ad2094b40d18ebc82948f87c0d128
Wakil Bouljoub <abouljoub@gmail.com>
2022-01-12 20:14:22 +0200

    Add user related components

--- src/Users.js
+++ src/Users.js
@@ -1,36 +1,37 @@
 import usersData from "./user-data";
 import { AiOutlineEdit, AiOutlineCopy, AiOutlineDelete } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 function Users() {
   return (
     <table className="table table-hover" id="farms-list">
       <thead>
         <tr>
-          <th scope="col">Name</th>
-          <th scope="col">Role</th>
-          <th scope="col">Email</th>
-          <th scope="col">Action</th>
+          <th>Name</th>
+          <th>Role</th>
+          <th>Email</th>
+          <th>Action</th>
         </tr>
       </thead>
       <tbody>
         {usersData.map((userData) => (
           <tr>
-            <th scope="row">{userData.name}</th>
-            <td scope="row">{userData.role}</td>
-            <td scope="row">{userData.email}</td>
+            <th>{userData.name}</th>
+            <td>{userData.role}</td>
+            <td>{userData.email}</td>
             <td>
-              <a href="#" className="btn">
+              <Link to={`/user/edit/${userData.name}`} className="btn">
                 <AiOutlineEdit className="me-2" />
                 Edit
-              </a>
-              <a href="#" className="btn">
+              </Link>
+              <Link to={`/user/new/${userData.name}`} className="btn">
                 <AiOutlineCopy className="me-2" />
                 Duplicate
-              </a>
-              <a href="#" className="btn">
+              </Link>
+              <Link to="#" className="btn">
                 <AiOutlineDelete className="me-2" />
                 Delete
-              </a>
+              </Link>
             </td>
           </tr>
         ))}
